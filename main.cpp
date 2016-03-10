11a5041de62f0c03f6e037c0c32d8e26b642946a
darkskeeper <dartskeeper@mail.ru>
2016-03-10 14:32:19 +0300

    something new

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <QCoreApplication>
+#include <QTextStream>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+//QTextStream cin(stdin);
+//QTextStream cout(stdout);
+
+/*QTextStream& qout()
+{
+    static QTextStream ts(stdout);
+    return ts;
+}*/
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    QString s1 = "Enter your name";
+    cout << s1.toStdString() << endl;
+    //QString s = cin.readLine();
+    //cout << "Your name is: " << s;
+    cout<<"Hello World";
+    return a.exec();
+}
